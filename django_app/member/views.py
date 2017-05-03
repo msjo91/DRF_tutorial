c1d060420e3c9eb3fac4165ba70ade509976e4e1
msjo91 <msjo91@gmail.com>
2017-05-03 18:10:41 +0900

    Create serializers, viewsets, and router for member app

--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.models import Group
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserSerializer, GroupSerializer
+from .models import MyUser
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = MyUser.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
