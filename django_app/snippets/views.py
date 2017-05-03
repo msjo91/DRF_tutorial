059a57f27606ffe4430ebcaca38b71507b4c8c49
msjo91 <msjo91@gmail.com>
2017-05-03 18:51:43 +0900

    Create snippet_list function

--- django_app/snippets/views.py
+++ django_app/snippets/views.py
@@ -1,3 +1,23 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from .serializers.modelserializers import SnippetSerializer
+from .models import Snippet
+
+
+@csrf_exempt
+def snippet_list(request):
+    if request.method == "GET":
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
