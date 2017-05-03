38d47a4d5aacfb32c609ed5e57092fb0211333b9
msjo91 <msjo91@gmail.com>
2017-05-03 19:00:24 +0900

    Reroute urls

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -25,6 +25,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^snippets/', include('snippets.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include('snippets.urls')),
 ]
