ef7d556a9f539c963448620107aeda4402f653f7
msjo91 <msjo91@gmail.com>
2017-05-04 20:57:32 +0900

    Create SnippetViewSet

---
+++ django_app/snippets/views/viewsets_cbv.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets, permissions, renderers
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+
+from ..permissions import IsOwnerOrReadOnly
+from ..serializers.modelserializers import SnippetSerializer
+from ..models import Snippet
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
