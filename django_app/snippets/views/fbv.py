4da2af0ff293d7eb280214bfee0a2eb27d8873e4
msjo91 <msjo91@gmail.com>
2017-05-04 14:20:00 +0900

    Divide views to fbv and cbv

--- django_app/snippets/views/fbv.py
+++ django_app/snippets/views/fbv.py
@@ -2,8 +2,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .serializers.modelserializers import SnippetSerializer
-from .models import Snippet
+from snippets.serializers.modelserializers import SnippetSerializer
+from snippets.models import Snippet
 
 
 @api_view(['GET', 'POST'])
