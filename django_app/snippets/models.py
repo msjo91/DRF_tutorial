95db2779cafcac68a40ff4b61fe219c9b75dfed4
msjo91 <msjo91@gmail.com>
2017-05-04 14:57:07 +0900

    Create owner, highlight fields, save function, and reset DB

--- django_app/snippets/models.py
+++ django_app/snippets/models.py
@@ -1,12 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_lexer_by_name
 
 from .choices import language_choices, style_choices
 
 LANGUAGE_CHOICES = language_choices()
 STYLE_CHOICES = style_choices()
 
+User = get_user_model()
+
 
 class Snippet(models.Model):
+    owner = models.ForeignKey(User, related_name='snippets', on_delete=models.CASCADE)
+    highlighted = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
@@ -16,3 +24,11 @@ class Snippet(models.Model):
 
     class Meta:
         ordering = ('created',)
+
+    def save(self, *args, **kwargs):
+        lexer = get_lexer_by_name(self.language)
+        linenos = self.linenos and 'table' or False
+        options = self.title and {'title': self.title} or {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
