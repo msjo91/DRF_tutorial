fe2bfd7351a0737b0c6914a6ad449a7fe40ea7d2
msjo91 <msjo91@gmail.com>
2017-05-04 14:41:11 +0900

    Create cbv using generics.APIView and reroute urls

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import cbv
+from .views import generics_cbv
 
 urlpatterns = [
-    url(r'^$', cbv.SnippetList.as_view()),
-    url(r'^(?P<pk>[0-9]+)/$', cbv.SnippetDetail.as_view()),
+    url(r'^$', generics_cbv.SnippetList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', generics_cbv.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
