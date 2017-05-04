dcd033dbb43f0768228ba6debce281530d36dec6
msjo91 <msjo91@gmail.com>
2017-05-04 14:32:33 +0900

    Create cbv and reroute urls

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from . import views
+from .views import cbv
 
 urlpatterns = [
-    url(r'^$', views.snippet_list),
-    url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^$', cbv.SnippetList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', cbv.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
